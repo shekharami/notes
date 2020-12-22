5254dc67654e5912b60b40dcc5678d7b1beeea5d
Amit Shekhar <shekharjvm@gmail.com>
2020-12-22 11:43:54 +0530

    added SIGTERM handler

--- server.js
+++ server.js
@@ -25,3 +25,18 @@ const port = process.env.PORT || 3000 ;
 const server = app.listen(port, () =>{
     console.log(`Server listening at port ${port}`);
 });
+
+process.on('unhandledRejection', (err)=>{
+    console.log( err.name, err.message);
+    console.log('Unhandled rejection... SHUTTING DOWN!');
+    server.close(()=>{
+        process.exit(1);// 0 for success, 1 for fail
+    });
+});
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM Recieved. Shutting down gracefully.');
+    server.close(() => {
+        console.log('Process Terminated');
+    });
+})
